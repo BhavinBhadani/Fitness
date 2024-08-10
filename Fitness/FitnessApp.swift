e851932ba6c7d7d5fa44c0e5a1abf989a67735dc
Bhavin B <bhavinbhadani@yahoo.in>
2024-08-10 09:39:56 +0530

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Bhavin Bhadani on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
